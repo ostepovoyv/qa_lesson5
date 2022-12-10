f25fb22c0168c265a8fd9eeb6654fd68a8b285a4
ostepovoyv <ov.stepovoy@gmail.com>
2022-12-10 23:13:44 +0600

    add allure

--- src/test/java/com/practiceform/test/config/TestBase.java
+++ src/test/java/com/practiceform/test/config/TestBase.java
@@ -2,9 +2,12 @@ package com.practiceform.test.config;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.logevents.SelenideLogger;
 import com.practiceform.test.testdata.PracticeFormData;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 
 public class TestBase {
 
@@ -15,6 +18,11 @@ public class TestBase {
 //        Configuration.browser= "firefox";
         Configuration.baseUrl= PracticeFormData.baseUrl;
 //        Configuration.holdBrowserOpen = true;
+    }
+
+    @BeforeEach
+    public void beforeEachTest(){
+        SelenideLogger.addListener("allure", new AllureSelenide());
     }
 
     @AfterAll
