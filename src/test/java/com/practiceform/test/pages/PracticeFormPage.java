571ddf8d09860914b812e7b75b6608349ccdd5ca
ostepovoyv <ov.stepovoy@gmail.com>
2022-12-27 14:02:48 +0600

    добавил @Step для методов

--- src/test/java/com/practiceform/test/pages/PracticeFormPage.java
+++ src/test/java/com/practiceform/test/pages/PracticeFormPage.java
@@ -32,7 +32,7 @@ public class PracticeFormPage {
             cityWrapper = $("#stateCity-wrapper"),
             submitButton = $("#submit");
 
-    @Step("Открываем форму {value}")
+    @Step("Открываем форму")
     public PracticeFormPage openPracticeFrom(String value){
         open("/automation-practice-form");
         formTitle.shouldHave(text(value));
@@ -41,57 +41,68 @@ public class PracticeFormPage {
         return this;
     }
 
+    @Step("Заполняем имя {value}")
     public PracticeFormPage setFirstName(String value){
         this.firstNameInput.setValue(value);
         return this;
     }
 
+    @Step("Заполняем фамилию {value}")
     public PracticeFormPage setLastName(String value){
         this.lastNameInput.setValue(value);
         return this;
     }
 
+    @Step("Заполняем email {value}")
     public PracticeFormPage setEmail(String value){
         this.emailInput.setValue(value);
         return this;
     }
 
+    @Step("Заполняем пол {value}")
     public PracticeFormPage setGender(String value){
         this.genderRadioButton.find(byText(value)).click();
         return this;
     }
 
+    @Step("Заполняем номер телефона {value}")
     public PracticeFormPage setUserNumber(String value){
         this.userNumberInput.setValue(value);
         return this;
     }
 
+    @Step("Заполняем ДР {dateOfBirthDay}.{dateOfBirthMonth}.{dateOfBirthYear}")
     public PracticeFormPage setDateOfBirth(String dateOfBirthMonth, String dateOfBirthYear, String dateOfBirthDay){
         dateOfBirthInput.click();
         calendarComponent.setBirthDate(dateOfBirthMonth, dateOfBirthYear, dateOfBirthDay);
         return this;
     }
 
+    @Step("Заполняем предметы {value}")
     public PracticeFormPage setSubjects(String value){
         subjectsInput.setValue(value).pressEnter();
         return this;
     }
 
+    @Step("Заполняем хобби {value}")
     public PracticeFormPage setHobbies(String value) {
         hobbiesWrapper.find(byText(value)).click();
         return this;
     }
 
+    @Step("Загружаем изображение")
     public PracticeFormPage uploadPicture(String path, String pictureName) {
         pictureLoader.uploadFile(new File(path + pictureName));
         return this;
     }
 
+    @Step("Заполняем адрес {value}")
     public PracticeFormPage setCurrentAddress(String value) {
         currentAddressTextarea.setValue(value);
         return this;
     }
 
+    @Step("Заполняем штат {state} и город {city}")
     public PracticeFormPage setStateAndCity(String state, String city) {
         stateSelect.scrollTo().click();
         stateCityWrapper.find(byText(state)).click();
@@ -100,6 +111,7 @@ public class PracticeFormPage {
         return this;
     }
 
+    @Step("нажимаем кнопку Submit")
     public PracticeFormPage clickSubmitButton() {
         submitButton.click();
         return this;
--- src/test/java/com/practiceform/test/pages/PracticeModalFormPage.java
+++ src/test/java/com/practiceform/test/pages/PracticeModalFormPage.java
@@ -2,6 +2,7 @@ package com.practiceform.test.pages;
 
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.SelenideElement;
+import io.qameta.allure.Step;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Condition.visible;
@@ -16,6 +17,7 @@ public class PracticeModalFormPage {
             modalFooter = $(".modal-footer"),
             modalTable = $(".table-responsive");
 
+    @Step("Проверяем текст заголовка на модальной форме")
     public PracticeModalFormPage verifyModalContent(String value){
         modalHeader.shouldBe(visible)
                 .shouldHave(Condition.text(value));
@@ -26,6 +28,7 @@ public class PracticeModalFormPage {
         modalTable.find(byText(label)).parent().shouldHave(text(value));
     }
 
+    @Step("Проверяем наличие Label и Values")
     public PracticeModalFormPage verifymodalBody(){
         modalBody.shouldHave(
                 text("Label"),
@@ -33,11 +36,13 @@ public class PracticeModalFormPage {
         return this;
     }
 
+    @Step("Проверяем соответствие введенных и отображаемых на форме данных {label} {values}")
     public PracticeModalFormPage verifyModalFormTableResults(String label, String values){
         verifyLabelWithValues(label,values);
         return this;
     }
 
+    @Step("Проверяем футер и закрываем модальную форму")
     public PracticeModalFormPage verifyModalFooter(){
         modalFooter.find("#closeLargeModal").shouldHave(text("Close")).click();
         return this;
